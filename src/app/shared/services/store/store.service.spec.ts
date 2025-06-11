5ea21c6e206be84e593ed66e0ae1093c1542e966
Ayem0 <baptiste.genin1@gmail.com>
2025-06-11 05:48:17 +0200

    store tests

--- src/app/shared/services/store/store.service.spec.ts
+++ src/app/shared/services/store/store.service.spec.ts
@@ -22,10 +22,19 @@ describe('Store', () => {
     expect(store).toBeTruthy();
   });
 
-  it('should set entity to store', () => {
+  it('should add entity to store', () => {
     const workspace: Workspace = { id: '1', name: 'test', color: 'test' };
     store.setEntity('workspaces', workspace);
     expect(store.workspaces().get(workspace.id)).toBe(workspace);
+  });
+
+  it('should add entities to store', () => {
+    const workspace1: Workspace = { id: '1', name: 'test', color: 'test' };
+    const workspace2: Workspace = { id: '2', name: 'test', color: 'test' };
+    store.setEntities('workspaces', [workspace1, workspace2]);
+    expect(store.workspaces().size).toBe(2);
+    expect(store.workspaces().get(workspace1.id)).toBe(workspace1);
+    expect(store.workspaces().get(workspace2.id)).toBe(workspace2);
   });
 
   it('should set relation to store', () => {
@@ -45,5 +54,47 @@ describe('Store', () => {
     expect(store.workspacesUsers().get(key)).toEqual(
       new Set<string>([...rel, rel2])
     );
+  });
+
+  it('should remove from existing relation to store', () => {
+    const key = 'key';
+    const rel1 = 'rel1';
+    const rel2 = 'rel2';
+
+    store.setRelation('workspacesUsers', key, [rel1, rel2]);
+    store.deleteFromRelation('workspacesUsers', key, rel1);
+
+    expect(store.workspacesUsers().get(key)).toEqual(new Set<string>([rel2]));
+  });
+
+  it('should remove entity from store', () => {
+    const workspace1: Workspace = { id: '1', name: 'test', color: 'test' };
+    const workspace2: Workspace = { id: '2', name: 'test', color: 'test' };
+    store.setEntities('workspaces', [workspace1, workspace2]);
+    store.deleteEntity('workspaces', workspace1.id);
+    expect(store.workspaces().size).toBe(1);
+    expect(store.workspaces().get(workspace1.id)).toBeUndefined();
+    expect(store.workspaces().get(workspace2.id)).toBe(workspace2);
+  });
+
+  it('should remove entities from store', () => {
+    const workspace1: Workspace = { id: '1', name: 'test', color: 'test' };
+    const workspace2: Workspace = { id: '2', name: 'test', color: 'test' };
+    const workspace3: Workspace = { id: '3', name: 'test', color: 'test' };
+    store.setEntities('workspaces', [workspace1, workspace2, workspace3]);
+    store.deleteEntities('workspaces', [workspace1.id, workspace3.id]);
+    expect(store.workspaces().size).toBe(1);
+    expect(store.workspaces().get(workspace1.id)).toBeUndefined();
+    expect(store.workspaces().get(workspace2.id)).toBe(workspace2);
+    expect(store.workspaces().get(workspace3.id)).toBeUndefined();
+  });
+
+  it('should clear store', () => {
+    const workspace1: Workspace = { id: '1', name: 'test', color: 'test' };
+    const workspace2: Workspace = { id: '2', name: 'test', color: 'test' };
+    const workspace3: Workspace = { id: '3', name: 'test', color: 'test' };
+    store.setEntities('workspaces', [workspace1, workspace2, workspace3]);
+    store.clear();
+    expect(store.workspaces().size).toBe(0);
   });
 });
