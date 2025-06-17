d40c65830c48952f037d74438ca1a49a1efdecce
Ayem0 <baptiste.genin1@gmail.com>
2025-06-17 14:39:41 +0200

    animations

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,19 +1,16 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
-  imports: [MatButtonModule, TranslateModule, MatIcon],
+  imports: [MatButtonModule, TranslateModule, MatIcon, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
   private readonly router = inject(Router);
-  public navigateToRegister() {
-    this.router.navigate(['/register']);
-  }
 }
