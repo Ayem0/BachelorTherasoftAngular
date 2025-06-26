5317638c9e23dba49185c57b3de0fa7562ea6381
Ayem0 <baptiste.genin1@gmail.com>
2025-06-26 11:44:24 +0200

    fix

--- src/app/features/workspace/models/workspace.ts
+++ src/app/features/workspace/models/workspace.ts
@@ -1,17 +1,36 @@
 import { FormControl } from '@angular/forms';
 import { FilterRelations } from '../../../shared/helpers/filter-relations.helper';
 import { Entity } from '../../../shared/models/entity';
+import {
+  EventCategory,
+  EventCategoryRelations,
+} from '../../event-category/models/event-category';
+import { Location, LocationRelations } from '../../location/models/location';
+import { Member, MemberRelations } from '../../member/models/member';
+import {
+  ParticipantCategory,
+  ParticipantCategoryRelations,
+} from '../../participant-category/models/participant-category';
+import {
+  Participant,
+  ParticipantRelations,
+} from '../../participant/models/participant';
 import { Slot, SlotRelations } from '../../slot/models/slot';
+import { Tag, TagRelations } from '../../tag/models/tag';
+import {
+  WorkspaceRole,
+  WorkspaceRoleRelations,
+} from '../../workspace-role/models/workspace-role';
 
-interface workspaceRelationsOptions {
+export interface WorkspaceRelations {
   slots?: Slot<SlotRelations>[];
-  workspaceRoles?: unknown;
-  members?: unknown;
-  tags?: unknown;
-  locations?: unknown;
-  participants?: unknown;
-  participantCategories?: unknown;
-  eventCategories?: unknown;
+  workspaceRoles?: WorkspaceRole<WorkspaceRoleRelations>[];
+  members?: Member<MemberRelations>[];
+  tags?: Tag<TagRelations>[];
+  locations?: Location<LocationRelations>[];
+  participants?: Participant<ParticipantRelations>[];
+  participantCategories?: ParticipantCategory<ParticipantCategoryRelations>[];
+  eventCategories?: EventCategory<EventCategoryRelations>[];
 }
 
 type BaseWorkspace = {
@@ -20,10 +39,8 @@ type BaseWorkspace = {
   description?: string;
 } & Entity;
 
-export type Workspace = BaseWorkspace &
-  FilterRelations<workspaceRelationsOptions>;
-
-export type StoreWorkspace = BaseWorkspace;
+export type Workspace<R extends WorkspaceRelations = {}> = BaseWorkspace &
+  FilterRelations<R>;
 
 export interface WorkspaceRequest {
   name: string;
